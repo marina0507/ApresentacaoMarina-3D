510fcd0df8a701fac6ad4a839010bf8d67331f48
marina0507 <163479053+marina0507@users.noreply.github.com>
2024-08-29 01:27:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const primeiroNome = "Marina";
+const sobrenome = "Lourenço";
+const idade = 17;
+const altura = 1.69;
+const massa = 59;
+const cidadeNatal = "São josé dos pinhais-PR";
+
+console.log("O meu primeiro nome é:")
+console.log(primeiroNome);
+
+console.log("O meu sobrenome é;")
+console.log(sobrenome);
+
+console.log("A minha idade é:")
+console.log(idade);
+
+console.log("A minha altura é:")
+console.log(altura);
+
+console.log("A minha massa é:")
+console.log(massa);
+
+console.log("A minha cidade Natal é:")
+console.log(cidadeNatal);
